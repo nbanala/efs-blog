14b90496354c181be1993ea24dd7bdfbc9c9c023
Naveen <nbanala@unomaha.edu>
2017-09-16 00:00:30 -0500

    Heroku Deployed

--- efsblog/settings.py
+++ efsblog/settings.py
@@ -11,11 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
-# Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -156,4 +152,8 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
